91625d9dcb5e57d8d29ba4e1873faea0c13e0ddf
lucasbanunes <lucasbanunes@gmail.com>
2025-11-22 23:54:31 -0300

    Removes unused code

--- even_flow/lightning.py
+++
@@ -1,9 +0,0 @@
-# from typing import Literal
-# import lightning as L
-
-
-# type DatasetType = Literal['train', 'val', 'test', 'predict']
-
-# def test_datamodule_loader(datamodule: L.LightningDataModule,
-#                            dataset: DatasetType) -> bool:
-#     try:
