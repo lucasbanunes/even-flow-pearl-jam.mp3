0fae5fa861df82577b7278cb46f8c2d49c209388
lucasbanunes <lucasbanunes@gmail.com>
2025-12-13 17:36:22 -0300

    Changes pytest mlflow path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pytest
+import mlflow
 
 
 @pytest.fixture
@@ -15,3 +16,8 @@ def test_dir() -> Path:
 @pytest.fixture
 def data_dir() -> Path:
     return Path(__file__).parent / "data"
+
+
+@pytest.fixture(autouse=True)
+def setup_mlflow_tracking(data_dir: Path):
+    mlflow.set_tracking_uri("file://" + str(data_dir / "mlruns"))
